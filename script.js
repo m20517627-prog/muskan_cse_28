8b5dc279ecaf2b4fb19dbc460029796fe6cd3231
Muskan <m20517627@gmail.com>
2026-02-24 14:59:39 +0530

    add file

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("MUSKAN");
+// var a=10;
+// var a=20;
+// console.log(a);
+/*var a=10;
+var b=20;
+var c=a+b;
+console.log(c);*/
+
+console.log("let introduction");
+let a=10;
+a=90;
+console.log(a);
+const x="hello";
+console.log(x);
