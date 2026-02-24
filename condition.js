25ea83a77fc8e777cb313628741beba907498103
Muskan <m20517627@gmail.com>
2026-02-24 15:26:57 +0530

    add file

---
+++ condition.js
@@ -0,0 +1,10 @@
+let temperature = 25;
+if(temperature>30){
+console.log("it's hot !");   
+}
+else if(temperature>20){
+    console.log("it's warm.");
+}
+else{
+    console.log("It's cold.");
+}
---
+++ switch.js
@@ -0,0 +1,24 @@
+let day=3;
+switch(day){
+    case 1:
+    console.log("Monday");
+    break;
+case 2:
+    console.log("Tuesday");
+    break;
+case 3:
+    console.log("wendesday");
+    break;
+
+case 4:
+    console.log("Thursday");
+    break;
+case 5:
+    console.log("Friday");
+    break;
+case 6:
+    console.log("Saturday");
+    break;
+case 7:
+    console.log("sunday");
+}
